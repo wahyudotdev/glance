f7aa4519cc5e20e2a5eb7c65679025f3ad28a004
GitHub Action <action@github.com>
2026-02-21 22:14:52 +0000

    docs: updated coverage badge [skip ci]

--- internal/client/exec.go
+++
@@ -1,5 +0,0 @@
-package client
-
-import "os/exec"
-
-var execCommand = exec.Command
--- internal/config/version.go
+++
@@ -1,5 +0,0 @@
-package config
-
-// Version is the current version of Glance.
-// It is set at build time via ldflags.
-var Version = "dev"
